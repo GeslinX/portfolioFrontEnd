99868d5f679da1883fa9465a2b5d112280bfa8b9
GeslinX <132654827+GeslinX@users.noreply.github.com>
2023-06-01 09:04:24 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import 'material-symbols';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
+import './index.css';
 
 function importAll(r) {
   let images = {};
