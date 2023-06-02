f9824660ac925d712c4cc63fd5cbf78804eb3518
GeslinX <132654827+GeslinX@users.noreply.github.com>
2023-06-02 15:15:21 +0200

    Create SkeletonProject.js

---
+++ src/components/SkeletonProject.js
@@ -0,0 +1,11 @@
+import React  from 'react'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+
+const SkeletonProject = () => (
+  <div>
+    <Skeleton height={590} width={460} />
+  </div>
+)
+
+export default SkeletonProject;
