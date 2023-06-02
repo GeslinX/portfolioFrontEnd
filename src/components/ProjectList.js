33547bd295080d6eb1469e978a7b4c647af0e99a
GeslinX <132654827+GeslinX@users.noreply.github.com>
2023-06-02 16:28:08 +0200

    Update ProjectList.js

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,60 +1,66 @@
-import React, { useEffect, useState, Suspense } from "react";
-import SkeletonProject from "./SkeletonProject";
+import React, { useEffect, useState } from "react";
+import Skeleton from 'react-loading-skeleton'
 import { Link } from "react-router-dom";
 import images from '../index';
-
-
-const Project = (props) => (
-  <Link to={`/project/${props.project._id}`}>
-    <div className="inline-block snap-start" role="listitem">
-        <div className="translate-x-[calc(max(1600px,100vw)/_2_-_790px)]">
-            <div className="cardsScroller mr-5 w-[350px] sm:w-[460px] flex overflow-hidden relative rounded-2xl shadow-[0_2px_10px_-1px_rgba(0,0,0,0.1)] whitespace-normal">
-                <img src={images[`${props.project.images}`]} alt={props.project.title} loading="lazy" width="480" height="610"/>
-                <div className="absolute p-10 bottom-0">
-                    <span style={{color: props.project.color}} className={'font-body px-3 py-1 bg-white font-black text-sm uppercase rounded-full'}>{props.project.tags}</span>
-                    <h2 className="font-body text-white font-bold text-3xl max-w-[285px]">{props.project.title}</h2>
-                </div>
-            </div>
-        </div>
-    </div>
-  </Link>
-);
+import 'react-loading-skeleton/dist/skeleton.css'
 
 export default function ProjectList() {
     const [projects, setProjects] = useState([]);
     // This method fetches the projects from the database.
+    const getProjects = () => {
+      fetch(`https://portfolio-xavier-backend.onrender.com/project`)
+      .then((res) => res.json())
+      .then((json) => setProjects(json))
+      .catch((e) => console.log(e))
+  }
     useEffect(() => {
-      async function getProjects() {
-        const response = await fetch(`https://portfolio-xavier-backend.onrender.com/project`);
-   
-        if (!response.ok) {
-          const message = `An error occurred: ${response.statusText}`;
-          window.alert(message);
-          return;
-        }
-   
-        const projects = await response.json();
-        setProjects(projects);
+      setTimeout(() => {
+        getProjects()
+      }, 5000)
+    }, [])
+
+    const Project = () => {
+      return (
+        <>
+          {projects.map((project) => {
+            return (
+              <Link to={`/project/${project._id}`}>
+                <div className="inline-block snap-start" role="listitem">
+                    <div className="translate-x-[calc(max(1600px,100vw)/_2_-_790px)]">
+                        <div className="cardsScroller mr-5 w-[350px] sm:w-[460px] flex overflow-hidden relative rounded-2xl shadow-[0_2px_10px_-1px_rgba(0,0,0,0.1)] whitespace-normal">
+                            <img src={images[`${project.images}`]} alt={project.title} loading="lazy" width="480" height="610"/>
+                            <div className="absolute p-10 bottom-0">
+                                <span style={{color: project.color}} className={'font-body px-3 py-1 bg-white font-black text-sm uppercase rounded-full'}>{project.tags}</span>
+                                <h2 className="font-body text-white font-bold text-3xl max-w-[285px]">{project.title}</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+              </Link>
+              )
+            })}
+          </>
+        )
       }
-   
-      getProjects();
-   
-      return;
-    }, [projects.length]);
-  
-   
-    // This method will map out the projects on the table
-    function projectList() {
-      return projects.map((project) => {
+      const showSkeleton = () => {
         return (
-          <Suspense fallback={<SkeletonProject />}>
-            <Project project={project} key={project._id} />
-          </Suspense>
-        );
-      });
-    }
-
-
+          <>
+            {Array(4)
+              .fill()
+              .map((project,index) => {
+                return (
+                  <div className="inline-block snap-start" role="listitem" key={index}>
+                    <div className="translate-x-[calc(max(1600px,100vw)/_2_-_790px)]">
+                        <div className="cardsScroller mr-5 w-[350px] sm:w-[460px] flex overflow-hidden relative rounded-2xl shadow-[0_2px_10px_-1px_rgba(0,0,0,0.1)] whitespace-normal">
+                          <Skeleton height={590} width={460} />
+                        </div>
+                    </div>
+                </div>
+                )
+              })}
+          </>
+        )
+      }
     return(
         <section id="projects">
             <div className="container mx-auto mb-12 flex px-2.5 pt-20 flex-col items-left">
@@ -62,7 +68,7 @@ export default function ProjectList() {
             </div>
             <div id="scollerContent">
                 <div className="w-full whitespace-nowrap inline-flex overflow-x-scroll snap-mandatory snap-x align-top overscroll-x-contain" id="scrollerPlatter" role="list">
-                  {projectList()}
+                  {projects.length > 0 ? Project() : showSkeleton()}
                 </div>
             </div>
         </section>
