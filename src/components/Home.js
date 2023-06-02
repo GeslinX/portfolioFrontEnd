540c54af962712673f987f8028193640d3daeaec
GeslinX <132654827+GeslinX@users.noreply.github.com>
2023-06-02 15:17:47 +0200

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,19 @@
 
 import React from "react";
-const Banner = React.lazy(() => import("./Banner"));
+import Banner from "./Banner";
+import { TabTitle } from "./TabTitle";
 const About = React.lazy(() => import("./About"));
 const ProjectList = React.lazy(() => import("./ProjectList"));
 const Skills = React.lazy(() => import("./Skills"));
 
 export default function Home() {
+  TabTitle('Xavier Geslin - Portfolio');
   return (
     <div id="home">
       <Banner />
       <About />
       <Skills />
-      <ProjectList />
+      <ProjectList />  
     </div>
   );
 }
