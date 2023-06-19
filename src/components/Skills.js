24b098ce501fb29c7bd4a7f2625c7ffe91115bb7
GeslinX <132654827+GeslinX@users.noreply.github.com>
2023-06-19 10:02:37 +0200

    Update Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -26,7 +26,7 @@ export default function Skills() {
                         <div className='flex-[2_2_100%] 2xl:flex-1'>
                             <h1 className='font-body text-3xl uppercase font-bold text-white'>Branding</h1>
                             <h2 className='font-title text-4xl text-orange-gx'>Le contenu adapté</h2>
-                            <p className="font-body text-xl font-regular text-white">Analyser, comprendre sa cible. C’est permettre à l’utilisateur d’accéder aux informations dont il a besoin et au bon moment, sublimer son interface n’en rendra sa navigation que meilleure. Je propose mes services en matière UI/UX Design afin de répondre au mieux aux besoins de mes clients.</p>
+                            <p className="font-body text-xl font-regular text-white">Dans un contexte Marketing où se distinguer de ses concurrents devient de plus en plus complexe. Il est important de travailler son image de marque. Instaurer une relation de confiance et fédérer une communauté soudée autour de sa marque. J'interviens en matière de création du contenu et construis une identité propre à votre marque dans laquelle l'utilisateur s'identifie.</p>
                         </div>
                     </div>
                     <div id="front" role="tabpanel" aria-labelledby="front-tab" label="Front Integrator" count="03">
@@ -36,7 +36,7 @@ export default function Skills() {
                         <div className='flex-[2_2_100%] 2xl:flex-1'>
                             <h1 className='font-body text-3xl uppercase font-bold text-white'>Front Integrator</h1>
                             <h2 className='font-title text-4xl text-orange-gx'>Code the web</h2>
-                            <p className="font-body text-xl font-regular text-white">Analyser, comprendre sa cible. C’est permettre à l’utilisateur d’accéder aux informations dont il a besoin et au bon moment, sublimer son interface n’en rendra sa navigation que meilleure. Je propose mes services en matière UI/UX Design afin de répondre au mieux aux besoins de mes clients.</p>
+                            <p className="font-body text-xl font-regular text-white">L'intégration Web orientée Front-end permet de mettre en valeur votre identité de marque, en collaboration avec le développeur, il fournit un projet visuel et interactif visible aux yeux de tous vos utilisateurs. En fonction de l'environnement existant de mes clients, je travaille avec différents CMS comme WordPress, Shopify, Salesforce, etc. Et utilise les langages HTML, CSS et JavaScript afin de fournir des solutions full responsive et optimiser pour à la WebPerformance.</p>
                         </div>
                     </div>
                 </Tabs>
