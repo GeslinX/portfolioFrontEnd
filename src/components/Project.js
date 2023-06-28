c42eba9d9129210bedaf8febcc320182c627749b
GeslinX <132654827+GeslinX@users.noreply.github.com>
2023-06-28 09:04:16 +0200

    Update Project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,6 +4,7 @@ import { HashLink as Link } from 'react-router-hash-link';
 import { TabTitle } from "./TabTitle";
 import { ArrowUturnLeftIcon } from '@heroicons/react/24/outline'
 import images from '../index';
+import Footer from './Footer';
 
 const Project = () => {
 
@@ -39,17 +40,20 @@ const Project = () => {
   
   TabTitle(`Xavier Geslin - Portfolio | ${activeProject.title}`);
     return (
-      <section className="pt-5">
-        <div className="container mx-auto mt-24 flex flex-col items-start max-w-[calc(1600px_-_300px)]">
-          <Link to="/#projects" className="myLink font-body text-center text-grey-gx hover:text-orange-gx hover:font-bold"><ArrowUturnLeftIcon className="inline-block h-5 w-5" aria-hidden="true" /> Retour</Link>
-        </div>
-        <div className="container mx-auto px-2.5 flex flex-col items-center max-w-[calc(1600px_-_300px)]">
-            <h1 className="font-body font-semibold pb-2.5 text-4xl text-grey-gx uppercase text-center">{activeProject.title}</h1>
-            <p className="font-body text-xl text-center">{activeProject.info}</p>
-            {activeProject.link ? <Link to={`${activeProject.link}`} target="_blank" className="myLink font-body text-xl text-center text-grey-gx hover:text-orange-gx hover:font-bold">Voir la page</Link> : null}
-            <img src={images[`${activeProject.projectimg}`]} alt={activeProject.title} className="my-8 rounded-3xl shadow-2xl"/>
-        </div>
-      </section>
+      <div>
+        <section className="pt-5">
+          <div className="container mx-auto mt-24 flex flex-col items-start max-w-[calc(1600px_-_300px)]">
+            <Link to="/#projects" className="myLink font-body text-center text-grey-gx hover:text-orange-gx hover:font-bold"><ArrowUturnLeftIcon className="inline-block h-5 w-5" aria-hidden="true" /> Retour</Link>
+          </div>
+          <div className="container mx-auto px-2.5 flex flex-col items-center max-w-[calc(1600px_-_300px)]">
+              <h1 className="font-body font-semibold pb-2.5 text-4xl text-grey-gx uppercase text-center">{activeProject.title}</h1>
+              <p className="font-body text-xl text-center">{activeProject.info}</p>
+              {activeProject.link ? <Link to={`${activeProject.link}`} target="_blank" className="myLink font-body text-xl text-center text-grey-gx hover:text-orange-gx hover:font-bold">Voir la page</Link> : null}
+              <img src={images[`${activeProject.projectimg}`]} alt={activeProject.title} className="my-8 rounded-3xl shadow-2xl"/>
+          </div>
+        </section>
+        <Footer />
+      </div>
     );
 }
 
